eb41d98d66c9447b3b00bf26f1c7fa2093d53ae5
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-05 19:37:14 +0530

    First Commit

---
+++ 08-SumOfNaturalNum/JavaBasic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasic{
+    public static void main(String arg[]){
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter The Number");
+      int number=sc.nextInt();
+      int sum=0;
+      int i=1;
+      while(i<=number)
+      {
+        sum +=i;
+        i++;
+      }
+        System.out.println("The Sum is: " + sum);
+    }
+}
