48679e93b0f7088afa6611b84449be6d8ea9590c
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-05 23:43:38 +0530

    First Commit

---
+++ 09-BreakStatement/JavaBasic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class JavaBasic{
+    public static void main(String arg[]){
+      Scanner sc= new Scanner(System.in);
+      for(int i=1;i<=4;i++)
+      {
+      if (i==3)
+        {
+          break;
+        }
+        System.out.println(i);
+    }
+       System.out.println("Out From The  Loop");
+
+  }
+
+}
