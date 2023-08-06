770bdd4fa9485610c7dea269881ab49b0eccc0c6
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-06 13:17:51 +0530

    First Commit

---
+++ 13-StarPattern(2)/JavaBasic.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+public class JavaBasic{
+  public static void main(String arg[])
+  {
+    Scanner sc=new Scanner(System.in);
+   int num=4;
+   for (int line=0;line<num;line++)
+   {
+    for(int star=1;star<num-line+1;star++)
+    {
+      System.out.print("*");
+
+    }
+    System.out.println();
+   }
+  }
+}
