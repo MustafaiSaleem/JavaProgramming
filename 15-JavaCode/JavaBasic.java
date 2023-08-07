39017d16b620313f2c445dd097f3608d892165b3
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-07 16:18:58 +0530

    First Commit

---
+++ 15-JavaCode/JavaBasic.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JavaBasic{
+  public static void Hello(){
+    for(int i=1;i<=10;i++)
+    {
+      System.out.println("Hello World");
+    }
+  }
+  public static void main(String arg[]){
+      Hello();
+  }
+}
