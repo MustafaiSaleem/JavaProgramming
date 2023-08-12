be8351ae4a2aad3a3c9c8db0aa66fe4960fb05cb
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-12 23:54:55 +0530

    First Commit

---
+++ 19-3GreatestNum/JavaBasic.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasic{
+  public static void main(String arg[]){
+    int a=100;
+    int b=40;
+    int c=30;
+    if (a>b)
+    {
+      System.out.println("A is greatest");
+    }
+    if(b>c)
+    {
+      System.out.println("B is greatest");
+    }
+    else
+    {
+      System.out.println("C is greatest");
+    }
+
+  }
+}
