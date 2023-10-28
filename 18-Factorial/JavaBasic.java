a1c1407f35c4bd60b4e18ef18ab4dd2d411b65d8
MustafaiSaleem <saleemraza1522@gmail.com>
2023-10-28 11:54:56 +0530

    First Commit

--- 18-Factorial/JavaBasic.java
+++ 18-Factorial/JavaBasic.java
@@ -12,3 +12,26 @@ public class JavaBasic{
     System.out.println(factorial(3));
   }
 }
+
+// USING BigInteger for Big number of result
+
+// import java.util.*;
+// import java.math.BigInteger;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int num = 100;
+
+//         for (int i = 1; i <= num; i++) {
+//             BigInteger fac = BigInteger.ONE; // Use BigInteger
+
+//             for (int j = 1; j <= i; j++) {
+//                 fac = fac.multiply(BigInteger.valueOf(j)); // Multiply using BigInteger
+//             }
+
+//             System.out.println("Factorial of " + i + " is: " + fac);
+//         }
+//     }
+// }
