00773f5c031f827789bfd55cd7d5673d316e2724
MustafaiSaleem <saleemraza1522@gmail.com>
2023-12-05 18:19:33 +0530

    First Commit

--- 12-StarPattern(1)/JavaBasic.java
+++ 12-StarPattern(1)/JavaBasic.java
@@ -15,3 +15,38 @@ public class JavaBasic{
     
   }
 }
+
+////////////////// With recurnsion //////////////////////////////
+
+// import java.util.*;
+// public class Solution{
+//     public static void starPattern(int row, int col, int size){
+//         if(row > size){
+//             return;
+//         }
+//         if(col < row){
+//             System.out.print("*");
+//             starPattern(row, col+1, size);
+//         }else{
+//             System.out.println();
+//             starPattern(row + 1, col = 0, size);
+//         }
+//     }
+//     public static void main(String arg[]){
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("\n Enter the size");
+//         int row = 0;
+//         int col = sc.nextInt();
+//         int size = col;
+        
+//         starPattern(row, col,size);
+//     }
+// }
+
+
+// output -:
+// *
+// **
+// ***
+// ****
