5bde41962322c93f8301f0635f58f4bc6bfbcda0
MustafaiSaleem <saleemraza1522@gmail.com>
2023-12-05 18:27:55 +0530

    First Commit

--- 04-ForLoop/JavaBasic.java
+++ 04-ForLoop/JavaBasic.java
@@ -1,11 +1,10 @@
-import java.util.*;
+
 public class JavaBasic{
+
     public static void main(String arg[]){
-        Scanner  sc= new Scanner (System.in);
-        int a=0,b=1,sum=0,i;
-        for (i=0;i<=10;i++)
+        for (int i=0;i<=10;i++)
         {
           System.out.println("Hello World");
         }
-    }
+ }
 }
